74f3d3eae27ee75df1fc7c0ca94cdaeaa5e9b581
balfoldi <balfoldi@yahoo.fr>
2020-11-16 14:35:56 +0100

    done

--- morpion.js
+++ morpion.js
@@ -1,8 +1,11 @@
 class Morpion {
 	constructor(player) {
-		this.player = player;
+		this.player = player
+		this.lastUndo = localStorage["lastUndo"] ? JSON.parse(localStorage["lastUndo"]) : []
+		this.history = localStorage["history"] ? JSON.parse(localStorage["history"]) : []
 		this.ia = (player == "J1") ? "J2" : "J1";
-		this.map = [];
+		this.iaLevel = localStorage["iaLevel"] ? localStorage["iaLevel"] : "normal"
+		this.map = []
 		for (let i = 0; i < 3; i++) {
 			this.map[i] = [];
 			for (let j = 0; j < 3; j++) {
@@ -11,6 +14,17 @@ class Morpion {
 			}
 		}
 		this.finish = false;
+		if (this.history.length) {
+			let i = 1
+			this.history.forEach((move) => {
+				let x = move[0]
+				let y = move[1]
+				let currentPlayer = i % 2 ? "J1" : "J2"
+				this.fillGrid(x, y, currentPlayer)
+				i++
+			})
+		}
+		this.updateIa(this.iaLevel)
 		if (this.ia === "J1")
 			this.iaTurn();
 	}
@@ -34,13 +48,14 @@ class Morpion {
 		return true;
 	}
 
-	fillGrid = (x, y, player) => {
-		const image = (player == this.player) ? 'croix' : 'rond';
+	fillGrid = (x, y, player = null) => {
+		let image = (player == this.player) ? 'croix' : 'rond';
 		const zone = this.getZone(x, y);
 
 		if (this.map[x][y] != "EMPTY")
 			return false;
 		this.map[x][y] = player;
+		this.history.push([x, y])
 		document.getElementById(zone).style.backgroundImage = `url(image-morpion/${image}.png)`;
 		document.getElementById(zone).className += " filled";
 		this.checking(player);
@@ -66,13 +81,14 @@ class Morpion {
 			two === five && two === eight && two != "EMPTY" ||
 			three === six && three === nine && three != "EMPTY") {
 			this.finish = true;
+			localStorage["lastUndo"] = []
+			localStorage["history"] = []
 			if (player == this.ia) {
 				document.getElementById('win').textContent = 'L\'IA a gagné !';
 			} else if (player == this.player) {
 				document.getElementById('win').textContent = 'Tu as battu l\'IA !';
 			}
-		}
-		else if (this.checkDraw()) {
+		} else if (this.checkDraw()) {
 			document.getElementById('win').textContent = "Vous êtes à égalité";
 			this.finish = true;
 		}
@@ -110,8 +126,12 @@ class Morpion {
 			return;
 		if (!this.fillGrid(x, y, this.player))
 			return alert('La case n\'est pas vide');
-		else if (!this.finish)
+		else if (!this.finish) {
 			this.iaTurn();
+		}
+		this.saveGame()
+		console.log(this.history)
+
 	}
 
 	minimax = (board, depth, isMaximizing) => {
@@ -157,27 +177,140 @@ class Morpion {
 		}
 	}
 
-	iaTurn = () => {
-		let depth = 0;
-		let bestScore = -Infinity;
-		let move;
-		for (let i = 0; i < 3; i++) {
-			for (let j = 0; j < 3; j++) {
-				if (this.map[i][j] == "EMPTY") {
-					this.map[i][j] = this.ia;
-					this.turn++;
-					let score = this.minimax(this.map, depth + 1, false);
-					this.map[i][j] = "EMPTY";
-					this.turn--;
-					if (score > bestScore) {
-						bestScore = score;
-						move = { i, j };
-					}
+	aiChecking = (map) => {
+		const one = [0, 0];
+		const two = [0, 1];
+		const three = [0, 2];
+		const four = [1, 0];
+		const five = [1, 1];
+		const six = [1, 2];
+		const seven = [2, 0];
+		const eight = [2, 1];
+		const nine = [2, 2];
+		const winLines = [
+			[one, two, three],
+			[one, four, seven],
+			[one, five, nine],
+			[two, five, eight],
+			[three, six, nine],
+			[three, five, seven],
+			[four, five, six],
+			[seven, eight, nine]
+		]
+		let result = false
+		winLines.forEach((winLine) => {
+			if (
+				winLine.filter((cords) => {
+					return map[cords[0]][cords[1]] === "J2"
+				}).length === 2
+			) {
+				console.log("one line can win")
+				const isFree = winLine.filter((cords) => {
+					return map[cords[0]][cords[1]] === "EMPTY"
+				})
+				console.log(isFree.length)
+				if (isFree.length === 1) {
+					console.log("i can play it")
+					result = isFree
 				}
 			}
+		})
+		return result
+	}
+
+	iaTurn = () => {
+		console.log("current IA level is " + this.iaLevel)
+		switch (this.iaLevel) {
+			case "easy":
+				let x = Math.floor(Math.random() * 3)
+				let y = Math.floor(Math.random() * 3)
+				while (!this.fillGrid(x, y, this.ia)) {
+					x = Math.floor(Math.random() * 3)
+					y = Math.floor(Math.random() * 3)
+				}
+				break;
+			case "normal":
+				const canWin = this.aiChecking(this.map)
+				if (canWin) {
+					this.fillGrid(canWin[0][0], canWin[0][1], this.ia)
+				} else {
+					let x = Math.floor(Math.random() * 3)
+					let y = Math.floor(Math.random() * 3)
+					while (!this.fillGrid(x, y, this.ia)) {
+						x = Math.floor(Math.random() * 3)
+						y = Math.floor(Math.random() * 3)
+					}
+				}
+				break
+			case "hard":
+				let depth = 0;
+				let bestScore = -Infinity;
+				let move;
+				for (let i = 0; i < 3; i++) {
+					for (let j = 0; j < 3; j++) {
+						if (this.map[i][j] == "EMPTY") {
+							this.map[i][j] = this.ia;
+							this.turn++;
+							let score = this.minimax(this.map, depth + 1, false);
+							this.map[i][j] = "EMPTY";
+							this.turn--;
+							if (score > bestScore) {
+								bestScore = score;
+								move = {
+									i,
+									j
+								};
+							}
+						}
+					}
+				}
+				this.fillGrid(move.i, move.j, this.ia);
+				break;
 		}
-		this.fillGrid(move.i, move.j, this.ia);
+
 	};
+
+	undo = () => {
+		this.history.splice(this.history.length - 2, 2).forEach((move) => {
+			let x = move[0]
+			let y = move[1]
+			this.map[x][y] = "EMPTY"
+			let zone = this.getZone(x, y)
+			document.getElementById(zone).style = ""
+			this.lastUndo.push(move)
+		})
+		this.saveGame()
+	}
+
+	redo = () => {
+		let player = "J1"
+		this.lastUndo.splice(this.lastUndo.length - 2, 2).forEach((move) => {
+			let x = move[0]
+			let y = move[1]
+			this.fillGrid(x, y, player)
+			player = "J2"
+		})
+		this.saveGame()
+	}
+
+	saveGame = () => {
+		localStorage["history"] = JSON.stringify(this.history)
+		localStorage["lastUndo"] = JSON.stringify(this.lastUndo)
+	}
+
+	updateIa = (level) => {
+		document.querySelector(`#ia${this.iaLevel.charAt(0).toUpperCase() + this.iaLevel.slice(1)}`).classList.remove("bouton-active")
+		this.iaLevel = level
+		localStorage['iaLevel'] = level
+		document.querySelector(`#ia${level.charAt(0).toUpperCase() + level.slice(1)}`).classList.add("bouton-active")
+	}
 }
 
-const morpion = new Morpion("J2");
+let morpion = new Morpion("J1")
+
+
+const newGame = () => {
+	localStorage["lastUndo"] = []
+	localStorage["history"] = []
+	location.reload()
+}
